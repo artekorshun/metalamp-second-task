c7acf5d24a78f89cacee559fdbac82cba66ef066
artekorshun <idd_qd@mail.ru>
2021-05-16 12:44:11 +0300

    Создание нового проекта, подготовка к работе

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// ---------- ENTRY POINT (Точка входа) ---------- //
+
+// Импорт JS
+import './js/main';
+
+// Импорт SCSS
+import '@styles/main.scss';
+
+// Импорт PUG
+import '@pages/index.pug';
