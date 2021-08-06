eaadd6ed8d72567a20b0344de3a091cd2db3a737
artekorshun <idd_qd@mail.ru>
2021-08-06 18:59:10 +0300

    pagination готова

--- src/pug/components/pagination/pagination.js
+++ src/pug/components/pagination/pagination.js
@@ -1,7 +1,7 @@
 import '~/js/libs/simplePagination/jquery.simplePagination.js';
 
 $(function () {
-	$('.pagination').pagination({
+	$('.pagination__simple-pagination').pagination({
 		items: 180,
 		itemsOnPage: 12,
 		displayedPages: 3,
